4aabba9019d4bb832d971786e25fa7c4a3e5a766
selveus1 <fs031090@gmail.com>
2018-02-12 19:20:05 -0500

    initial commit

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,11 +8,65 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var numRows = 0
+    var todos = [String]()
+    @IBOutlet weak var todoTable: UITableView!
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return numRows
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        if numRows > 0{
+            cell.textLabel?.text = String( todos[indexPath.row] )
+        }else{
+            cell.textLabel?.text = String( "" )
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete{
+            print("removing \(indexPath.row)")
+            if numRows > 0{
+                todos.remove(at: indexPath.row)
+                numRows = todos.count
+                UserDefaults.standard.set(todos, forKey: "todoList")
+                todoTable.reloadData()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        let listObject = UserDefaults.standard.object(forKey: "todoList")
+//        if let array = listObject as? NSArray as? [String]{
+//            print("in viewDidLoad --> \(array) ")
+//            numRows = array.count
+//            todos = array
+//        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let listObject = UserDefaults.standard.object(forKey: "todoList")
+        if let array = listObject as? NSArray as? [String]{
+            print("in viewDidAppear --> \(array) ")
+            numRows = array.count
+            todos = array
+            todoTable.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +74,6 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,8 +8,39 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func addBtnPressed(_ sender: Any) {
+        let listObject = UserDefaults.standard.object(forKey: "todoList")
+        
+        if let array = listObject as? NSArray as? [String]{
+            print(array)
+            var updatedArray = array
+            
+            if let userInput = textField.text{
+                let input = String(userInput)
+               // print(input)
+                updatedArray.append(input)
+                UserDefaults.standard.set(updatedArray, forKey: "todoList")
+            }
+            
+        }else{
+            print(textField.text)
+            
+            if let userInput = textField.text{
+                let input = String(userInput)
+                var updatedArray = [input]
+                UserDefaults.standard.set(updatedArray, forKey: "todoList")
+                
+            }
+
+        }
+        
+        textField.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +49,15 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
